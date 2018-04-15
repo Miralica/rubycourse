06b52f927347fdde01a9415998a3f3d140ec5a43
Miralica <bearsorokin@mail.com>
2018-04-15 14:46:18 +0300

    task1

--- lesson3/File.rb
+++
@@ -1,5 +0,0 @@
-puts "Hi. Fill it in : your name, your secondname and how old are you years"
-you_name = gets.chomp #name
-you_sname = gets.chomp #secname
-you_old = gets.chomp #old
-puts 'Yours name is:' + name +", your secondname :" + you_sname +", and you're" + how_old + " years old"
---
+++ lesson3/Task1.rb
@@ -0,0 +1,12 @@
+def abc
+	puts "Please enter your phone number and I'll tell you about it."
+	number = gets.chomp.to_s
+	c = []
+	c = number.split
+	c = с.reverse
+	c = c.first
+	puts c
+	puts "Length of your number : " + number.length.to_s
+	puts "And the last digit of your number is "
+end
+abc
