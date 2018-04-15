93042313a6b2de9503262f6f0783c6f89e0dbcad
Miralica <bearsorokin@mail.com>
2018-04-15 10:14:04 +0300

    task

--- lesson3/File.rb
+++ lesson3/File.rb
@@ -1,1 +1,5 @@
-puts "Hi."
+puts "Hi. Fill it in : your name, your secondname and how old are you years"
+you_name = gets.chomp #name
+you_sname = gets.chomp #secname
+you_old = gets.chomp #old
+puts 'Yours name is:' + name +", your secondname :" + you_sname +", and you're" + how_old + " years old"
